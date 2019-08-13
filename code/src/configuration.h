47c128ee0433d567a5ae1cd92121fac7469b573e
Xose Pérez <xose.perez@gmail.com>
2019-08-13 12:13:49 +0200

    Set message to sleep delay to 500ms

--- code/src/configuration.h
+++ code/src/configuration.h
@@ -42,9 +42,9 @@ void ttn_register(void (*callback)(uint8_t message));
 
 #define DEBUG_PORT              Serial      // Serial debug port
 #define SERIAL_BAUD             115200      // Serial debug baud rate
-#define SLEEP_BETWEEN_MESSAGES  1           // Do sleep between messages
 #define SEND_INTERVAL           30000       // Sleep for these many millis
-#define MESSAGE_TO_SLEEP_DELAY  2000        // Time after message before going to sleep
+#define SLEEP_BETWEEN_MESSAGES  1           // Do sleep between messages
+#define MESSAGE_TO_SLEEP_DELAY  500         // Time after message before going to sleep
 #define LOGO_DELAY              3000        // Time to show logo on first boot
 #define LORAWAN_PORT            10          // Port the messages will be sent to
 #define LORAWAN_CONFIRMED_EVERY 0           // Send confirmed message every these many messages (0 means never)
