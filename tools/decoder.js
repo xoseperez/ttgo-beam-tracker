c68db17d0ebb9e6535ab52b0f414130073c1a147
Xose Pérez <xose.perez@gmail.com>
2019-09-03 10:14:50 +0200

    Update javascript decoder to support frames with no LUA_COMPATIBILITY_MODE (default)

--- tools/decoder.js
+++ tools/decoder.js
@@ -29,7 +29,25 @@ function toBool(bytes) {
   return (1 === bytes[0]);
 }
 
-function unpack(bytes) {
+// Types:
+var MESSAGE_PACK_NULL = 0;
+var MESSAGE_PACK_BOOLEAN = 1;
+var MESSAGE_PACK_SHORT = 2;
+var MESSAGE_PACK_INTEGER = 3;
+var MESSAGE_PACK_LONG = 4;
+var MESSAGE_PACK_FLOAT = 5;
+var MESSAGE_PACK_TEXT = 6;
+
+var code_types_lua = [
+  MESSAGE_PACK_FLOAT, MESSAGE_PACK_LONG, MESSAGE_PACK_NULL, 
+  MESSAGE_PACK_BOOLEAN, MESSAGE_PACK_TEXT, MESSAGE_PACK_SHORT,
+  MESSAGE_PACK_INTEGER
+];
+
+function unpack(bytes, lua) {
+
+  // default to normal compatibility
+  lua = typeof lua !== 'undefined' ? lua : false;
 
   // array to hold values
   var data = [];
@@ -50,27 +68,32 @@ function unpack(bytes) {
 
   // decode data
   for (var i=0; i<size; i++) {
+    
     var type = types[i];
-    if (0 === type) {
-      data.push(toNumber(bytes.slice(count,count+4)));
-      count += 4;
-    } else if (1 === type) {
-      data.push(toInteger(bytes.slice(count,count+4), 4));
-      count += 4;
-    } else if (5 === type) {
-      data.push(toInteger(bytes.slice(count,count+2), 2));
-      count += 2;
-    } else if (6 === type) {
-      data.push(toInteger(bytes.slice(count,count+1), 1));
-      count += 1;
-    } else if (3 === type) {
+    if (lua) type = code_types_lua[type];
+
+    if (MESSAGE_PACK_NULL === type) {
+    } else if (MESSAGE_PACK_BOOLEAN === type) {
       data.push(toBool(bytes.slice(count,count+1)));
       count += 1;
-    } else if (4 === type) {
+    } else if (MESSAGE_PACK_SHORT === type) {
+      data.push(toInteger(bytes.slice(count,count+1), 1));
+      count += 1;
+    } else if (MESSAGE_PACK_INTEGER === type) {
+      data.push(toInteger(bytes.slice(count,count+2), 2));
+      count += 2;
+    } else if (MESSAGE_PACK_LONG === type) {  
+      data.push(toInteger(bytes.slice(count,count+4), 4));
+      count += 4;
+    } else if (MESSAGE_PACK_FLOAT === type) {
+      data.push(toNumber(bytes.slice(count,count+4)));
+      count += 4;
+    } else if (MESSAGE_PACK_TEXT === type) {
       var s = toString(bytes.slice(count));
       data.push(s);
       count += (s.length + 1);
     }
+
   }
 
   return data;
@@ -85,7 +108,7 @@ function Decoder(bytes, port) {
 
   // GPS info
   if (port == 10) {
-    var gps = unpack(bytes);
+    var gps = unpack(bytes, false); // set to "true" if using LUA_COMPATIBILITY_MODE in the encoder
     decoded.latitude = parseFloat(gps[0].toFixed(6));
     decoded.longitude = parseFloat(gps[1].toFixed(6));
     decoded.altitude = parseFloat(gps[2].toFixed(2));
